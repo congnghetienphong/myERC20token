9abf99f270858515e140d1bf23ccb9d03ac1446e
Xun <54058060+xuhengxun@users.noreply.github.com>
2021-04-26 19:53:59 +0800

    Update IERC20Extended.sol

--- Interface/IERC20Extended.sol
+++ Interface/IERC20Extended.sol
@@ -10,14 +10,29 @@ import "./_IERC20.sol";
  
 interface IERC20Extended is IERC20 {
     /**
-    * @dev Interface for the optional extended functions from the ERC20 standard.
-    * 
-    * Returns the name of the token.
-    */
+     * @dev Interface for the optional extended functions from the ERC20 standard.
+     * 
+     * Returns the name of the token.
+     */
     function name() external view returns (string memory);
-    // Returns the symbol of the token.
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
     function symbol() external view returns (string memory);
-    // Returns the decimals places of the token.
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
     function decimals() external view returns (uint8);
     
  }
