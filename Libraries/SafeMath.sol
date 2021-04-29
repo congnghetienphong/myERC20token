cbd09dce267974dfd719a14781bdc17ab992fff6
Xun <54058060+xuhengxun@users.noreply.github.com>
2021-04-29 19:25:26 +0800

    Update SafeMath.sol

--- Libraries/SafeMath.sol
+++ Libraries/SafeMath.sol
@@ -4,16 +4,40 @@ pragma solidity >=0.7.0 <0.9.0;
 
 // A library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
 library SafeMath {
-    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        require((c=a+b)>=a, 'ds-math-mul-overflow');
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c>=a, 'ds-math-mul-overflow');
+        return c;
     }
     
-    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        require((c=a-b)<=a, 'ds-math-sub-underflow');
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a - b;
+        require(c<=a, 'ds-math-sub-underflow');
+        return c;
     }
     
-    function mul(uint a, uint b) internal pure returns (uint c) {
-        require(b==0||(c=a*b)/b==a, 'ds-math-mul-overflow');
+    function mul(uint a, uint b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        require(b==0||c/b==a, 'ds-math-mul-overflow');
+        return c;
+    }
+    
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, 'ds-math-div-overflow');
+        uint256 c = a / b;
+        return c;
     }
     
     function min(uint256 a, uint256 b) internal pure returns (uint256 c) {
