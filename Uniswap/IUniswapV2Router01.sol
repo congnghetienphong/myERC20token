5de8e2f56ccc0d2a07e61aacf63228f2a0783fe2
Xun <54058060+xuhengxun@users.noreply.github.com>
2021-05-16 17:30:28 +0800

    Update and rename IUniswapV2Router01 to IUniswapV2Router01.sol

--- Uniswap/IUniswapV2Router01.sol
+++ Uniswap/IUniswapV2Router01.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.6.2;
 
 interface IUniswapV2Router01 {
     function factory() external pure returns (address);
